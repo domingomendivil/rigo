2997f29fe77f41c1b3377dbc596e112fde14e56f
Domingo <domingo.mendivil@gmail.com>
2017-06-05 10:09:03 -0300

    commiting

--- src/main/java/com/mag/dao/DAO.java
+++ src/main/java/com/mag/dao/DAO.java
@@ -2,4 +2,5 @@ package com.mag.dao;
 
 public interface DAO {
 
+	public <T> T getById(Object id,Class<T> a);
 }
--- src/main/java/com/mag/dao/DAOImpl.java
+++ src/main/java/com/mag/dao/DAOImpl.java
@@ -1,5 +1,20 @@
 package com.mag.dao;
 
+import javax.management.Query;
+
+
 public class DAOImpl implements DAO {
+	
+	public <T> T getById(Object id,Class<T> a){
+		
+		/**private static final String PERSISTENCE_UNIT_NAME = "User";
+	    private static EntityManagerFactory factory;
+		
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        // Read the existing entries and write to console
+        Query q = em.createQuery("SELECT u FROM User u");**/
+		return null;
+	}
 
 }
