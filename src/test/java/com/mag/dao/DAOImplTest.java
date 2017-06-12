af39a15810a571f19391d7d154cfa17849b6a2c9
Domingo <domingo.mendivil@gmail.com>
2017-06-12 15:01:16 -0300

    added

--- src/test/java/com/mag/dao/DAOImplTest.java
+++ src/test/java/com/mag/dao/DAOImplTest.java
@@ -1,5 +1,13 @@
 package com.mag.dao;
 
+import com.mag.beans.User;
+
 public class DAOImplTest {
+	
+	private DAOImpl dao = new DAOImpl();
+	
+	public void testUser(){
+		dao.getById("d0178",User.class);
+	}
 
 }
