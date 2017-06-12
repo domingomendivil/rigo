1bd87211c14c72a91604818394174ab705ad6433
Domingo <domingo.mendivil@gmail.com>
2017-06-12 15:59:09 -0300

    user

--- src/main/java/com/mag/beans/User.java
+++ src/main/java/com/mag/beans/User.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -22,6 +24,8 @@ public class User {
 	private String password;
 	
 	@ManyToMany
+	@JoinTable(name="user_roles",
+	joinColumns = {@JoinColumn(name="username", referencedColumnName="username")})
 	private Set<Rol> roles;
 
 	public String getUserName() {
