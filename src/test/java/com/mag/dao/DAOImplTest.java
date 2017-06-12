2f94cc8ae5f1ef3175691c40849c07a1608b00c4
Domingo <domingo.mendivil@gmail.com>
2017-06-12 15:07:47 -0300

    added new tests

--- src/test/java/com/mag/dao/DAOImplTest.java
+++ src/test/java/com/mag/dao/DAOImplTest.java
@@ -1,13 +1,11 @@
 package com.mag.dao;
 
-import com.mag.beans.User;
-
 public class DAOImplTest {
 	
 	private DAOImpl dao = new DAOImpl();
 	
 	public void testUser(){
-		dao.getById("d0178",User.class);
+		//dao.getById("d0178",User.class);
 	}
 
 }
