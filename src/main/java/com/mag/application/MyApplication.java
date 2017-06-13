517b04655528daa247b0817dacf355e7eac98ec3
Domingo <domingo.mendivil@gmail.com>
2017-06-13 15:52:29 -0300

    added authorizationfilter

--- src/main/java/com/mag/application/MyApplication.java
+++ src/main/java/com/mag/application/MyApplication.java
@@ -3,10 +3,13 @@ package com.mag.application;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
+import com.mag.filters.AuthorizationFilter;
+
 public class MyApplication extends ResourceConfig {
 	public MyApplication() {
         register(new MyApplicationBinder());
         packages(true, "com.mag.rest").register(RolesAllowedDynamicFeature.class);
+        register(AuthorizationFilter.class);
         System.out.println("dynamic feature added");
     }
 }
